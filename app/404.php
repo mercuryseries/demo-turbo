5a601adeb4aac37ecd01743f1928d9abe414a065
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-04-15 07:25:42 +0200

    btn-link, btn-primary css classes

--- app/404.php
+++ app/404.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>404 Not Found</title>
-    
+
     <link rel="stylesheet" href="css/app.css">
     <script type="module" src="https://cdn.skypack.dev/@hotwired/turbo"></script>
 </head>
@@ -15,7 +15,7 @@
         <div class="jumbotron">
             <h1>404 Not Found 😜</h1>
             <p>
-                Please go back <a href="/">home</a>.
+                Please go back <a href="/" class="btn-link">home</a>.
             </p>
         </div>
     </main>
