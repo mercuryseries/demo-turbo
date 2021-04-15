ffe536ba60c8add3d40c6f14c563794779a6f154
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-04-15 07:19:16 +0200

    link css in 404 template

--- app/404.php
+++ app/404.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>404 Not Found</title>
-
+    
+    <link rel="stylesheet" href="css/app.css">
     <script type="module" src="https://cdn.skypack.dev/@hotwired/turbo"></script>
 </head>
 <body>
